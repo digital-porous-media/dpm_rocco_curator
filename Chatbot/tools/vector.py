12344d690554584ec011acaaed75331869ae7d33
znowacek <n.zachary@wustl.edu>
2025-08-02 12:53:09 -0500

    Update vector.py
    Accommodating the tool for both keyword and chunking techniques used in the Neo4j

--- Chatbot/tools/vector.py
+++ Chatbot/tools/vector.py
@@ -4,26 +4,52 @@ from graph import graph
 from langchain_neo4j import Neo4jVector
 from langchain.embeddings.base import Embeddings
 
-# Create the Neo4jVector
-neo4jvector = Neo4jVector.from_existing_index(
-    embeddings,                              # (1)
-    graph=graph,                             # (2)
-    index_name="datasetDescription",                 # (3)
-    node_label="Dataset",                      # (4)
-    text_node_property="description",               # (5)
-    embedding_node_property="descriptionEmbedding", # (6)
-    retrieval_query="""
-RETURN
-    node.description AS text,
-    score,
-    {
-        title: node.title,
-        sampleTitles: [(sample)-[:PART_OF]->(node) | sample.title ],
-        datasetNumber: node.datasetNumber,
-        doi: node.doi
-    } AS metadata
 """
-)
+Uncomment the code below if descriptions in the Neo4j are embedded using the keywords model (JsonToNeo4jwKeywords.ipymb)
+"""
+# neo4jvector = Neo4jVector.from_existing_index(
+#     embeddings,                              # (1)
+#     graph=graph,                             # (2)
+#     index_name="datasetDescription",                 # (3)
+#     node_label="Dataset",                      # (4)
+#     text_node_property="description",               # (5)
+#     embedding_node_property="descriptionEmbedding", # (6)
+#     retrieval_query="""
+# RETURN
+#     node.description AS text,
+#     score,
+#     {
+#         title: node.title,
+#         sampleTitles: [(sample)-[:PART_OF]->(node) | sample.title ],
+#         datasetNumber: node.datasetNumber,
+#         doi: node.doi
+#     } AS metadata
+# """
+# )
+
+"""
+Uncomment the code below if descriptions in the Neo4j are embedded using the chunking model (JsonToNeo4jwChunking.ipymb)
+"""
+# neo4jvector = Neo4jVector.from_existing_index(
+#     embeddings,                              # (1)
+#     graph=graph,                             # (2)
+#     index_name="datasetDescription",         # (3)
+#     node_label="Chunk",                      # (4)
+#     text_node_property="text",               # (5)
+#     embedding_node_property="embedding", # (6)
+#     retrieval_query="""
+# RETURN
+#     node.text AS text,
+#      {
+#         description: [ (node)-[:CHUNK]->(d:Description) | d.description ],
+#         datasetNumber: [ (node)-[:CHUNK]->(d:Description) -[:DESCRIPTION_FOR]-> (ds:Dataset) | ds.datasetNumber ],
+#         doi: [ (node)-[:CHUNK]->(d:Description) -[:DESCRIPTION_FOR]-> (ds:Dataset) | ds.doi ],
+#         sampleTitles: [ (node)-[:CHUNK]->(d:Description) -[:DESCRIPTION_FOR]-> (ds:Dataset) <-[:PART_OF]- (s:Sample) | s.title ]
+#     } AS metadata,
+#     score
+# """
+# )
+
 # Create the retriever
 retriever = neo4jvector.as_retriever()
 # Create the prompt
